aceff2e79abb29b6abb5ec364b3230b0fc87cb3a
Dhyey Manish Rajani <86003669+DhyeyR-007@users.noreply.github.com>
2026-04-16 02:29:36 +1000

    Delete rplidar_lcm_driver directory

--- rplidar_lcm_driver/services/install_rplidar_service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  # Quit on error.
-
-SRV_NAME=mbot-rplidar-driver
-
-sudo cp $SRV_NAME.service /etc/systemd/system/$SRV_NAME.service
-sudo systemctl daemon-reload
-sudo systemctl enable $SRV_NAME.service
